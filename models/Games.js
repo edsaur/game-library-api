47c32910bcd71591ee8368bdf24bd27e3c6c1deb
Edzer Dionido <jeddionido26@gmail.com>
2025-04-28 14:06:33 +0800

    fix some validation

--- models/Games.js
+++ models/Games.js
@@ -11,19 +11,20 @@ const gameSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please add a genre"],
         trim: true,
-        maxLength: [50, "Genre cannot be more than 50 characters"],
+        maxLength: [100, "Genre cannot be more than 100 characters"],
     },
     platform: {
         type: String,
         required: [true, "Please add an platform"],
         trim: true,
-        maxLength: [50, "Platform cannot be more than 50 characters"],
+        maxLength: [200, "Platform cannot be more than 200 characters"],
     },
     releaseYear: {
         type: Number,
         required: [true, "Please add a price"],
         trim: true,
-        maxLength: [20, "Price cannot be more than 20 characters"],
+        minLength: [4, "Release year cannot be less than 4 characters"],
+        maxLength: [4, "Release year cannot be more than 4 characters"],
     },
     description: {
         type: String,
