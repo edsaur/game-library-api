b4d7374b78b9051625e971579be8a71519e3bf74
Edzer Dionido <jeddionido26@gmail.com>
2025-04-28 13:52:40 +0800

    add api route for games

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import authRouter from "./routes/AuthRoutes.js";
+import gameRouter from "./routes/GameRoutes.js";
 
 
 const app = express();
@@ -14,15 +15,16 @@ if (!process.env.DATABASE_URL) {
     process.exit(1);
 }
 
-
+// Connect to MongoDB
 mongoose.connect(process.env.DATABASE_URL).then(() => {
     console.log("connected to database");
 }).catch((error) => {
     console.log(error);
 });
 
-
+//  API Routes
 app.use("/api/auth", authRouter);
+app.use("/api/games", gameRouter);
 
 // Global error-handling middleware
 app.use((err, req, res, next) => {
